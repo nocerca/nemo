234fa009cf2e7cf5c433148c914ec70afd444959
IconMatthew <jadaevmatvei@gmail.com>
2025-03-15 23:44:16 +0300

    Добавление для конфигурации Liquibase

---
+++ yclients-integration/src/main/java/no/cerca/config/LiquibaseConfiguration.java
@@ -0,0 +1,26 @@
+package no.cerca.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by jadae on 05.03.2025
+ */
+@Configuration
+public class LiquibaseConfiguration {
+
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(changeLog);
+        return liquibase;
+    }
+}
