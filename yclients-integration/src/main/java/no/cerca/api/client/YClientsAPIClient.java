d050b48d52b8e75a43210024d82091d3a2259c33
IconMatthew <jadaevmatvei@gmail.com>
2025-03-30 00:04:27 +0300

    Добавление методов для отправки email и sms рассылки

--- yclients-integration/src/main/java/no/cerca/api/client/YClientsAPIClient.java
+++ yclients-integration/src/main/java/no/cerca/api/client/YClientsAPIClient.java
@@ -13,10 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by jadae on 13.03.2025
