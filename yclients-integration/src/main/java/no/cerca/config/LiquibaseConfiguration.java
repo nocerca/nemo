fc3542079ebe4e990682964cc96bcd1d90a5b541
IconMatthew <jadaevmatvei@gmail.com>
2025-04-08 01:28:53 +0300

    Добавление конфигурации миграций для интеграционных тестов

--- yclients-integration/src/main/java/no/cerca/config/LiquibaseConfiguration.java
+++ yclients-integration/src/main/java/no/cerca/config/LiquibaseConfiguration.java
@@ -4,6 +4,7 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
@@ -17,10 +18,21 @@ public class LiquibaseConfiguration {
     private String changeLog;
 
     @Bean
+    @Profile("!test")
     public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource);
         liquibase.setChangeLog(changeLog);
         return liquibase;
+    }
+
+    @Bean
+    @Profile("test")
+    public SpringLiquibase liquibaseStub() {
+        return new SpringLiquibase() {
+            @Override
+            public void afterPropertiesSet() {
+            }
+        };
     }
 }
