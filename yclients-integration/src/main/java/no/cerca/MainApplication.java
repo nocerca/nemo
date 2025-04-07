db99d653f4aca1dad343eb7e6adb29a486a248df
IconMatthew <jadaevmatvei@gmail.com>
2025-04-08 01:28:22 +0300

    Установка настроек миграций для точки входа в модуль

--- yclients-integration/src/main/java/no/cerca/MainApplication.java
+++ yclients-integration/src/main/java/no/cerca/MainApplication.java
@@ -2,9 +2,10 @@ package no.cerca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-liquibase.properties")
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
