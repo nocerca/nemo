073e1deed36824efea220f89a90a0f86aa62cc97
IconMatthew <jadaevmatvei@gmail.com>
2025-04-17 21:38:08 +0300

    Добавление класса точки входа в программу

---
+++ bot/src/main/java/no/cerca/VkTeamsBotApplication.java
@@ -0,0 +1,11 @@
+package no.cerca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkTeamsBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VkTeamsBotApplication.class, args);
+    }
+}
