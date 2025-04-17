e9204e5c91fcb142dfebbee9b0f850a5b8d347fd
IconMatthew <jadaevmatvei@gmail.com>
2025-04-17 21:04:24 +0300

    Добавление класса сессии для бота

---
+++ bot/src/main/java/no/cerca/util/UserSession.java
@@ -0,0 +1,54 @@
+package no.cerca.util;
+
+import no.cerca.entities.Auth;
+import no.cerca.state.BotState;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jadae on 17.04.2025
+ */
+public class UserSession {
+    private final String userId;
+    private Auth auth;
+    private BotState state;
+    private Map<String, Object> context;
+
+    public UserSession(String userId) {
+        this.userId = userId;
+        this.context = new HashMap<>();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
+
+    public BotState getState() {
+        return state;
+    }
+
+    public void setState(BotState state) {
+        this.state = state;
+    }
+
+    public Object getContext(String key) {
+        return context.get(key);
+    }
+
+    public void putContext(String key, Object value) {
+        context.put(key, value);
+    }
+
+    public void clearContext() {
+        context.clear();
+    }
+}
